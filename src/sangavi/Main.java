800d1744f3bdc5542394b7b655cd8a4a40909cb4
SangaviDurairaj <77475808+SangaviDurairaj@users.noreply.github.com>
2021-01-15 18:03:44 +0530

    Update Main.java

--- src/sangavi/Main.java
+++ src/sangavi/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi sangavi.....");
+		System.out.println("hi goashan this is sangavi.....");
 	}
 
 }
